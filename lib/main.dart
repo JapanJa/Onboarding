f740d5a5266fd5e8fdd5691ae2786bc87619e1e5
Patara Wongsanga <121413751+JapanJa@users.noreply.github.com>
2026-05-18 01:28:45 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
 import 'screens/onboarding_screen.dart';
-// import 'sign_in/sign_in1.dart';
-
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ONBOARD',
+      title: 'PawGuide',
       theme: ThemeData.light(useMaterial3: true),
-      home: OnBoardingScreen(),
+      home: const OnBoardingScreen(),
     );
-    
   }
 }
